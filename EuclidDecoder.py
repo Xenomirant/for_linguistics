931f08b1669a12ccc6cf35f3ffcd1a7d86632bf0
gisly <gisly@list.ru>
2020-05-25 22:16:54 +0300

    added a new bad symbol

--- EuclidDecoder.py
+++ EuclidDecoder.py
@@ -9,35 +9,39 @@ import xml.etree.ElementTree as ET
 def decode_file(filename):
     log_filename = filename + '_processing.log'
     with open(log_filename, 'w', encoding='utf-8') as log_out:
-        #парсим файл как XML
+        # парсим файл как XML
         tree = ET.parse(filename)
-        #получаем абзацы
+        # получаем абзацы
         paragraphs = tree.findall('.//body/para')
         for paragraph in paragraphs:
-            #получаем предложения
+            # получаем предложения
             sentences = paragraph.findall('./se')
             for sentence in sentences:
                 if sentence.text:
                     try:
-                        #читаем текст как windows-1251
+                        # читаем текст как windows-1251
                         element_text_decoded = bytes(sentence.text, 'cp1251').decode('utf-8')
-                        #записываем в XML
+                        # записываем в XML
                         sentence.text = element_text_decoded
                     except Exception as e:
                         log_out.write("error for paragraph %s (sentence %s): %s : %s\r\n" %
-                              (paragraph.attrib['id'], sentence.attrib['variant_id'], sentence.text, e))
-                      
+                                      (paragraph.attrib['id'], sentence.attrib['variant_id'], sentence.text, e))
+
                         letter_to_replace = 'Р'
                         text_cut = sentence.text.replace(letter_to_replace, '$!$')
                         letter_to_replace2 = 'вЂ'
                         text_cut = text_cut.replace(letter_to_replace2, '$_$')
 
-                        element_text_decoded = bytes(text_cut, 'cp1251').decode('utf-8').\
+                        #кривой символ
+                        if text_cut.endswith('.В'):
+                            text_cut = text_cut[0:-1]
+
+                        element_text_decoded = bytes(text_cut, 'cp1251').decode('utf-8'). \
                             replace('$!$', 'И'). \
                             replace('$_$', ' ')
                     sentence.text = element_text_decoded
                     print(paragraph.attrib['id'], sentence.attrib['variant_id'])
-        #пишем результат
+        # пишем результат
         tree.write(filename + '_out.xml', encoding='utf-8')
 
 
